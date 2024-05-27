db1a6f505ac4a759b1f5ac0f1f75f6ab2a7c58cd
Achraf <achraf.sani@esi.ac.ma>
2024-05-27 14:52:54 +0100

    Initial commit

---
+++ logo-esi/assets/script.js
@@ -0,0 +1,40 @@
+// var fileUpload = document.getElementById("file_upload");
+// fileUpload.addEventListener("change", function(){
+// 	var fileSvg = fileUpload.files[0];
+// 	var reader = new FileReader();
+// 	reader.readAsText(fileSvg);	
+// 	reader.onload = function(){
+// 		document.getElementById("svg_preview").innerHTML = reader.result;
+// 		document.getElementById("svg_form").style.display = "block";
+// 	}
+// });
+
+
+if(document.getElementById("resize_value").value==0){
+	document.getElementById("download_wrapper").style.display = "none";
+
+}
+
+var playGround = function(){
+	var resizeValue = document.getElementById("resize_value").value;
+	document.getElementsByTagName("svg")[0].setAttribute("id","svg_resize");
+	document.getElementsByTagName("svg")[0].setAttribute("width", resizeValue+"px");
+	// document.getElementsByTagName("svg")[0].setAttribute("height", resizeValue);
+	document.getElementById("download_wrapper").style.display = "block";
+	var fileRename = "Logo_ESI-" + resizeValue + "px.png";
+	document.getElementById("data").setAttribute("download", fileRename);
+}
+
+// SVG to DATA URL Library
+
+function exportResizedSvg() {
+                var svg = document.getElementById("svg_resize");
+				var img = document.getElementById("fromcanvas");
+				svg.toDataURL("image/png", {
+					callback: function(data) {
+						img.setAttribute("src", data)
+						var a = document.querySelector("#data")
+						a.href = data
+					}
+				})
+			}
